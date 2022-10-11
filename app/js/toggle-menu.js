e7e3d42424691d2e17082746bf8430fee82b4abc
geoknee <georgeknee@googlemail.com>
2022-10-11 10:22:34 +0100

    yarn prettier:write

--- app/js/toggle-menu.js
+++ app/js/toggle-menu.js
@@ -1,14 +1,14 @@
 function toggleMenu() {
-  const menuBtn = document.querySelector('.menu-btn');
-  const closeBtn = document.querySelector('.mobile-menu__close');
-  const mobileMenu = document.querySelector('.mobile-menu');
+  const menuBtn = document.querySelector(".menu-btn");
+  const closeBtn = document.querySelector(".mobile-menu__close");
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-  menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('mobile-menu--open');
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("mobile-menu--open");
   });
 
-  closeBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('mobile-menu--open');
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("mobile-menu--open");
   });
 }
 
